95c864199ba891b058604ebf1bc1657a32caf9ac
luiz eduardo sagaz carvalho <masterdudu123@hotmail.com>
2022-07-11 20:43:58 -0300

    react funcionando

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Header from "./components/header";
+import NotificationButton from "./components/notificationButton";
+import SalesCard from "./components/salesCard/index";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 5)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+          <div>
+            <Header/>
+            <SalesCard/>
+          </div>
+          )
 }
 
 export default App
